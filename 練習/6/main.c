5ed532f1dbea3cddae39090f80f85eb29c57e78b
B10856060 <64625152+B10856060@users.noreply.github.com>
2020-06-24 01:05:25 +0800

    Add files via upload

--- 練習/6/main.c
+++ 練習/6/main.c
@@ -4,30 +4,34 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x,y;
-	printf("請輸入x,y座標:");
-	scanf("%d,%d",&x,y);
 	
-	if(x==0 && y==0){
-		printf("(0,0)原點");
-	}
-	else if(x>0 && y>0){
+	float x,y;
+	
+	printf("請輸入x座標:");
+	scanf("%f",&x);
+	printf("請輸入y座標:");
+	scanf("%f",&y);
+
+	if(x>0 && y>0){
 		printf("第一象限");
 	}
-	else if(x<0 && y>0){
-		printf("第二象限");
-	}
-	else if(x<0 && y<0){
-		printf("第三象限");
-	}
-	else if(x>0 && y<0){
-		printf("第四象限");
-	}
-	else if(x!=0 && y==0){
-		printf("x軸");
-	}
-	else if(x==0 && y!=0){
-		printf("y軸");
-	}
+		else if(x<0 && y>0){
+			printf("第二象限");
+		}
+			else if(x<0 && y<0){
+				printf("第三象限");
+			}
+				else if(x>0 && y<0){
+					printf("第四象限");
+				}
+					else if(x!=0 && y==0){
+						printf("x軸");
+					}
+						else if(x==0 && y!=0){
+							printf("y軸");
+						}
+							else if(x==0 && y==0){
+								printf("(0,0)原點");
+							}
 	return 0;
 }
